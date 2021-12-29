4ed21ae0a625d47daaa760239be545a946761b62
Leon-Gee <oscar.meza.leon@gmail.com>
2021-12-29 10:22:24 -0700

    condicionals

---
+++ condicionales.py
@@ -0,0 +1,16 @@
+#age = int(input("write your age: "))
+
+#if age > 17:
+#	print("You are an adult")
+#else:
+#	print("You are under-age")
+
+
+number = int(input("Write a number"))
+
+if number > 5:
+	print("over 5")
+elif number == 5:
+	print("equal 5")
+else:
+	print("minor 5")
