86ddf717f384d26ed3fbccbd1e9972989ffe7318
Leon-Gee <oscar.meza.leon@gmail.com>
2022-01-01 16:56:37 -0700

    loops

---
+++ loops.py
@@ -0,0 +1,21 @@
+#for i in range(1,1000):
+#	print(pow(i,2))
+
+
+#def run():
+#	limit = 1000
+#	count = 0
+#	pow_2 = 2**count
+#	while pow_2 < 1000:	
+#		print("2 powered " + str(count) + " equals " + str(pow_2))
+#		count += 1
+#		pow_2 = 2**count
+#
+
+def run():
+	for i in range(1,1000):
+		print(i)
+
+
+if __name__ == "__main__":
+	run()
