55babbacc2c1d5f491086b78d62943b336dc5d8c
Leon-Gee <oscar.meza.leon@gmail.com>
2022-01-04 11:36:26 -0700

    turtle and files refactoring

---
+++ crud_flask_course/turtle_square.py
@@ -0,0 +1,19 @@
+import turtle
+
+window = turtle.Screen()
+our_turtle = turtle.Turtle()
+
+our_turtle.forward(100)
+our_turtle.right(90)
+
+our_turtle.forward(100)
+our_turtle.right(90)
+
+our_turtle.forward(100)
+our_turtle.right(90)
+
+our_turtle.forward(100)
+our_turtle.right(90)
+
+
+window.mainloop()
